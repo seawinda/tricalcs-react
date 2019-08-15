293a435323cfafd1cbcf155696fd8db3cb092008
seawinda <lekochka@gmail.com>
2019-08-15 10:59:54 +0300

    Добавила папку docs

---
+++ docs/precache-manifest.e9fcf6832eccecffea8f2d4abc2f2646.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "896fd1e6a0bdd9d4ec18",
+    "url": "/tricalcs/static/js/runtime~main.a46d7232.js"
+  },
+  {
+    "revision": "413cf2e745c847236bab",
+    "url": "/tricalcs/static/js/main.c0c027b4.chunk.js"
+  },
+  {
+    "revision": "70a28e9dc15859917234",
+    "url": "/tricalcs/static/js/2.b838580a.chunk.js"
+  },
+  {
+    "revision": "70a28e9dc15859917234",
+    "url": "/tricalcs/static/css/2.3137c38d.chunk.css"
+  },
+  {
+    "revision": "e1192d229da3be894928ce4c362c2cb4",
+    "url": "/tricalcs/index.html"
+  }
+];
